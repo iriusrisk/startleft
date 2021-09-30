1170857eda61236a4901e29347f32ccd71971f4e
David Fernandez <dfernandez@continuumsecurity.net>
2021-09-30 12:23:05 +0200

    New PUT endpoint and their tests

--- startleft/api/controllers/cloudformation/cloudformation_controller.py
+++ startleft/api/controllers/cloudformation/cloudformation_controller.py
@@ -10,13 +10,15 @@ from startleft.messages import messages
 PREFIX = '/api/beta/startleft/cloudformation'
 URL = ''
 RESPONSE_BODY = {}
-RESPONSE_STATUS_CODE = 201
+RESPONSE_STATUS_CODE_POST = 201
+RESPONSE_STATUS_CODE_PUT = 204
 
 router = APIRouter(
     prefix=PREFIX,
     tags=["cloudformation"],
     responses={
         201: {"description": messages.PROJECT_SUCCESSFULLY_CREATED},
+        204: {"description": messages.PROJECT_SUCCESSFULLY_CREATED},
         400: {"description": messages.BAD_REQUEST,
               "model": ErrorResponse},
         401: {"description": messages.UNAUTHORIZED_EXCEPTION,
@@ -26,7 +28,7 @@ router = APIRouter(
 )
 
 
-@router.post(URL, status_code=RESPONSE_STATUS_CODE)
+@router.post(URL, status_code=RESPONSE_STATUS_CODE_POST)
 def cloudformation(cft_file: UploadFile = File(..., description="File that contains the CloudFormation Template"),
                    type: FileType = Form(..., description="Format of the CloudFormation Template"),
                    id: str = Form(..., description="ID of the new project"),
@@ -39,12 +41,35 @@ def cloudformation(cft_file: UploadFile = File(..., description="File that conta
                    ):
     # Add custom mapping provided by customer
     cf_mapping_files = paths.default_cf_mapping_files
-    if len(mapping_file.filename) != 0:
+    if mapping_file and len(mapping_file.filename) != 0:
         cf_mapping_files = [mapping_file.file]
 
     # Run client
     cli.inner_run(type=type, map=cf_mapping_files, otm='threatmodel.otm', name=name, id=id,
                   ir_map=paths.default_ir_map, recreate=1, irius_server=ApiConfig.get_iriusrisk_server(),
+                  api_token=api_token, filename=[cft_file.file])
+
+    return RESPONSE_BODY
+
+
+@router.put('/projects/{project_id}', status_code=RESPONSE_STATUS_CODE_PUT)
+def cloudformation(project_id: str,
+                   cft_file: UploadFile = File(..., description="File that contains the CloudFormation Template"),
+                   type: FileType = Form(..., description="Format of the CloudFormation Template"),
+                   api_token: str = Header(None, description="IriusRisk API token"),
+                   mapping_file: UploadFile = File(None, description="File that contains the mapping between AWS "
+                                                                     "components and IriusRisk components. Providing "
+                                                                     "this file will completely override default values"
+                                                   )
+                   ):
+    # Add custom mapping provided by customer
+    cf_mapping_files = paths.default_cf_mapping_files
+    if mapping_file and len(mapping_file.filename) != 0:
+        cf_mapping_files = [mapping_file.file]
+
+    # Run client
+    cli.inner_run(type=type, map=cf_mapping_files, otm='threatmodel.otm', name=None, id=project_id,
+                  ir_map=paths.default_ir_map, recreate=0, irius_server=ApiConfig.get_iriusrisk_server(),
                   api_token=api_token, filename=[cft_file.file])
 
     return RESPONSE_BODY
--- tests/api/controllers/cloudformation/test_cloudformation_controller.py
+++ tests/api/controllers/cloudformation/test_cloudformation_controller.py
@@ -14,8 +14,12 @@ webapp = fastapi_server.initialize_webapp(IRIUSRISK_URL)
 client = TestClient(webapp)
 
 
-def get_url():
+def get_url_post():
     return cloudformation_controller.PREFIX + cloudformation_controller.URL
+
+
+def get_url_put(project_id):
+    return cloudformation_controller.PREFIX + cloudformation_controller.URL + f"/projects/{project_id}"
 
 
 class TestCloudFormationController:
@@ -23,7 +27,7 @@ class TestCloudFormationController:
     def test_create_project_api_token_not_set(self):
         files = {'cft_file': open(test_resource_paths.example_json, 'r')}
         body = {'id': 'project_id', 'name': 'project_name', 'type': 'JSON'}
-        response = client.post(get_url(), files=files, data=body)
+        response = client.post(get_url_post(), files=files, data=body)
         assert response.status_code == 401
 
     @pytest.mark.parametrize('project_id,project_name,project_type,cft_file',
@@ -38,7 +42,7 @@ class TestCloudFormationController:
         # When I do post on cloudformation endpoint
         files = {'cft_file': cft_file}
         headers = {'api-token': 'fd865d7d-3e8a-4499-a3e2-937de70bf5c2'}
-        response = client.post(get_url(), files=files, data=body, headers=headers)
+        response = client.post(get_url_post(), files=files, data=body, headers=headers)
 
         # Then
         assert response.status_code == 400
@@ -63,8 +67,60 @@ class TestCloudFormationController:
         files = {'cft_file': open(test_resource_paths.example_json, 'r')}
         body = {'id': f'{project_id}', 'name': 'project_A_name', 'type': 'JSON'}
         headers = {'api-token': 'fd865d7d-3e8a-4499-a3e2-937de70bf5c2'}
-        response = client.post(get_url(), files=files, data=body, headers=headers)
+        response = client.post(get_url_post(), files=files, data=body, headers=headers)
 
         # Then
-        assert response.status_code == cloudformation_controller.RESPONSE_STATUS_CODE
+        assert response.status_code == cloudformation_controller.RESPONSE_STATUS_CODE_POST
+        assert response.json() == cloudformation_controller.RESPONSE_BODY
+
+    def test_update_project_api_token_not_set(self):
+        # Given a project_id
+        project_id: str = 'project_A_id'
+
+        files = {'cft_file': open(test_resource_paths.example_json, 'r')}
+        body = {'name': 'project_name', 'type': 'JSON'}
+        response = client.put(get_url_put(project_id), files=files, data=body)
+        assert response.status_code == 401
+
+    @pytest.mark.parametrize('project_id,project_type,cft_file',
+                             [(None, 'type', open(test_resource_paths.example_json, 'r')),
+                              ('id', 'type', open(test_resource_paths.example_json, 'r')),
+                              ('id', None, open(test_resource_paths.example_json, 'r')),
+                              ('id', 'type', None)])
+    def test_update_project_validation_error(self, project_id: str, project_type: str, cft_file):
+        # Given a body
+        body = {'type': project_type}
+
+        # When I do post on cloudformation endpoint
+        files = {'cft_file': cft_file}
+        headers = {'api-token': 'fd865d7d-3e8a-4499-a3e2-937de70bf5c2'}
+        response = client.put(get_url_put(project_id), files=files, data=body, headers=headers)
+
+        # Then
+        assert response.status_code == 400
+
+    @responses.activate
+    def test_update_not_existing_project_ok(self):
+        # Given a project_id
+        project_id: str = 'project_A_id'
+
+        # And a IriusRisk response mock with the list of existing projects
+        responses.add(responses.GET, IRIUSRISK_URL + IriusRisk.API_PATH + '/products',
+                      json=[{'ref': 'project_B_id'}, {'ref': 'project_C_id'}], status=200)
+
+        # And a IriusRisk response mock with the creation of the project
+        responses.add(responses.POST, IRIUSRISK_URL + IriusRisk.API_PATH + '/products/upload/',
+                      status=200)
+
+        # And a response mock with the execution of the rules
+        responses.add(responses.PUT, IRIUSRISK_URL + IriusRisk.API_PATH + f"/rules/product/{project_id}")
+
+        # When I do post on cloudformation endpoint
+        files = {'cft_file': open(test_resource_paths.example_json, 'r')}
+        body = {'name': 'project_A_name', 'type': 'JSON'}
+        headers = {'api-token': 'fd865d7d-3e8a-4499-a3e2-937de70bf5c2'}
+        response = client.put(get_url_put(project_id), files=files, data=body, headers=headers)
+
+        # Then
+        assert response.status_code == cloudformation_controller.RESPONSE_STATUS_CODE_PUT
         assert response.json() == cloudformation_controller.RESPONSE_BODY
