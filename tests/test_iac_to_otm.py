cc6f51482d4b855609cc13d887c6e5e0828c6e13
David Fernandez <dfernandez@continuumsecurity.net>
2021-12-13 09:51:28 +0000

    [BLZ-56] mapping for singleton component tags

--- tests/test_iac_to_otm.py
+++ tests/test_iac_to_otm.py
@@ -40,13 +40,46 @@ class TestApp:
         mapping_filename = test_resource_paths.default_mapping
         iac_to_otm = IacToOtm('name', 'id')
         iac_to_otm.run('Cloudformation', mapping_filename, 'threatmodel.otm', filename)
+
         assert iac_to_otm.source_model.otm
         assert len(iac_to_otm.otm.trustzones) == 1
         assert len(iac_to_otm.otm.components) > 1
         assert len(iac_to_otm.otm.dataflows) == 1
+
         assert list(filter(lambda obj: obj.name == 'DummyCertificate', iac_to_otm.otm.components))
         assert list(filter(lambda obj: obj.name == 'kms (grouped)', iac_to_otm.otm.components))
         assert list(filter(lambda obj: obj.name == 'sns (grouped)', iac_to_otm.otm.components))
         assert list(filter(lambda obj: obj.name == 'cloudwatch (grouped)', iac_to_otm.otm.components))
-        assert list(filter(lambda obj: obj.name == 'Systems Manager from VPCEndpoint', iac_to_otm.otm.components))
+        assert list(filter(lambda obj: obj.name == 'api-gateway (grouped)', iac_to_otm.otm.components))
+        assert list(filter(lambda obj: obj.name == 'DynamoDB from VPCEndpoint', iac_to_otm.otm.components))
+        assert list(filter(lambda obj: obj.name == 'Systems Manager from VPCEndpoint (grouped)', iac_to_otm.otm.components))
         assert list(filter(lambda obj: obj.name == 'API gateway data flow from DummyApiAuthorizer', iac_to_otm.otm.dataflows))
+
+        assert list(filter(lambda obj: obj.name == 'DummyCertificate'
+                                       and "AWS::CertificateManager::Certificate" in obj.tags
+                                       and len(obj.tags) == 1, iac_to_otm.otm.components))
+        assert list(filter(lambda obj: obj.name == 'kms (grouped)'
+                                       and "DummyTableKey (AWS::KMS::Key)" in obj.tags
+                                       and "DummyCanaryBucketKey (AWS::KMS::Key)" in obj.tags
+                                       and len(obj.tags) == 2, iac_to_otm.otm.components))
+        assert list(filter(lambda obj: obj.name == 'sns (grouped)'
+                                       and "DummySubscription (AWS::SNS::Subscription)" in obj.tags
+                                       and "DummyTopic (AWS::SNS::Topic)" in obj.tags
+                                       and len(obj.tags) == 2, iac_to_otm.otm.components))
+        assert list(filter(lambda obj: obj.name == 'cloudwatch (grouped)'
+                                       and "DummyCWAlarm (AWS::CloudWatch::Alarm)" in obj.tags
+                                       and "DummyLogGroupA (AWS::Logs::LogGroup)" in obj.tags
+                                       and "DummyLogGroupB (AWS::Logs::LogGroup)" in obj.tags
+                                       and len(obj.tags) == 3, iac_to_otm.otm.components))
+        assert list(filter(lambda obj: obj.name == 'api-gateway (grouped)'
+                                       and "DummyApiAuthorizer (AWS::ApiGateway::Authorizer)" in obj.tags
+                                       and "DummyApiGwKdsRestApi (AWS::ApiGateway::RestApi)" in obj.tags
+                                       and len(obj.tags) == 2, iac_to_otm.otm.components))
+        assert list(filter(lambda obj: obj.name == 'Systems Manager from VPCEndpoint (grouped)'
+                                       and "DummyVPCssm (AWS::EC2::VPCEndpoint)" in obj.tags
+                                       and "DummyVPCssmmessages (AWS::EC2::VPCEndpoint)" in obj.tags
+                                       and len(obj.tags) == 2, iac_to_otm.otm.components))
+        assert list(filter(lambda obj: obj.name == "DynamoDB from VPCEndpoint"
+                                       and "DummyVPCdynamodb (AWS::EC2::VPCEndpoint)" in obj.tags
+                                       and len(obj.tags) == 1, iac_to_otm.otm.components))
+
