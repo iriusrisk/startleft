fd61b0df2fa8f5fd11a1f4faa8003349ec09b74a
David Fernandez <dfernandez@continuumsecurity.net>
2022-02-09 16:22:40 +0000

    [BLZ-291] first refactor

--- tests/resources/test_resource_paths.py
+++ tests/resources/test_resource_paths.py
@@ -1,11 +1,12 @@
 import os
 
 cloudformation_for_mappings_tests_json = os.path.dirname(__file__) + '/cloudformation_for_mappings_tests.json'
-cloudformation_for_security_group_tests_json = os.path.dirname(__file__)+'/cloudformation_for_security_group_tests.json'
-cloudformation_for_security_group_tests_json_2 = os.path.dirname(__file__)\
-                                                 + '/cloudformation_for_security_group_tests_simplified_2.json'
+cloudformation_for_security_group_tests_json_a = os.path.dirname(__file__)\
+                                                 + '/cloudformation_for_security_group_tests_a.json'
+cloudformation_for_security_group_tests_json_b = os.path.dirname(__file__)\
+                                                 + '/cloudformation_for_security_group_tests_b.json'
 cloudformation_for_security_groups_mapping = os.path.dirname(__file__)\
-                                             +'/cloudformation_for_security_group_tests_mapping_definitions.yaml'
+                                             + '/cloudformation_for_security_group_tests_mapping_definitions.yaml'
 default_mapping = os.path.dirname(__file__)+'/../../startleft/config/default-cloudformation-mapping.yaml'
 example_json = os.path.dirname(__file__) + '/example.json'
 example_yaml = os.path.dirname(__file__) + '/example.yaml'
