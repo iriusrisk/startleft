438022793a2e3be4bbb49a76143ad2669477d257
David Fernandez <dfernandez@continuumsecurity.net>
2022-03-21 12:55:57 +0000

    [OPT-78] merge from feature/OPT-70

--- startleft/api/controllers/iac/iac_create_otm_controller.py
+++ startleft/api/controllers/iac/iac_create_otm_controller.py
@@ -42,7 +42,7 @@ def iac(iac_file: UploadFile = File(..., description="File that contains the IaC
     logger.info(f"POST request received for creating new project with id {id} and name {name} from IaC {iac_type} file")
 
     logger.info("Parsing IaC file to OTM")
-    otm_project = OtmProject.from_iac_file_to_otm_stream(id, name, FILE_TYPE, [cft_file.file]
+    otm_project = OtmProject.from_iac_file_to_otm_stream(id, name, iac_type, [iac_file.file]
                                                          , [mapping_file.file] if mapping_file else [])
 
     return Response(status_code=201, media_type="application/json", content=otm_project.get_otm_as_json())
