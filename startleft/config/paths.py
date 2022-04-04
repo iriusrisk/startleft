40301afa6ed03b4efdbb015d438afeed77465c02
David Antolin Alvarez <dantolin@iriusrisk.com>
2022-04-04 18:13:12 +0200

    [OPT-115] Renamed default-terraform-aws-mapping.yaml to default-terraform-mapping.yaml

--- startleft/config/paths.py
+++ startleft/config/paths.py
@@ -1,4 +1,4 @@
 import os
 
 default_cf_mapping_file = [os.path.join(os.path.dirname(__file__), 'default-cloudformation-mapping.yaml')]
-default_tf_aws_mapping_file = [os.path.join(os.path.dirname(__file__), 'default-terraform-aws-mapping.yaml')]
+default_tf_aws_mapping_file = [os.path.join(os.path.dirname(__file__), 'default-terraform-mapping.yaml')]
