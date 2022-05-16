c04a2e06e0e13ec9c6ba667e468f06b5918eccb9
Pablo Santamarta Esteban <psantamarta@iriusrisk.com>
2022-05-16 17:40:12 +0200

    [OPT-181] Support for default and custom mapping files for Visio

--- startleft/api/controllers/diagram/diag_create_otm_controller.py
+++ startleft/api/controllers/diagram/diag_create_otm_controller.py
@@ -2,10 +2,9 @@ import logging
 
 from fastapi import APIRouter, File, UploadFile, Form, Response
 
-from startleft.provider import Provider
 from startleft.api.controllers.otm_controller import RESPONSE_STATUS_CODE, PREFIX, controller_responses
-
 from startleft.project.otm_project import OtmProject
+from startleft.provider import Provider
 
 URL = '/diagram'
 
@@ -23,13 +22,16 @@ def diagram(diag_file: UploadFile = File(..., description="File that contains th
             diag_type: Provider = Form(..., description="Type of Diagram File: VISIO"),
             id: str = Form(..., description="ID of the new project"),
             name: str = Form(..., description="Name of the new project"),
-            mapping_file: UploadFile = File(..., description="File that contains the mapping between IaC "
-                                                             "resources and threat model resources.")):
+            default_mapping_file: UploadFile = File(..., description="File that contains the default mapping file"),
+            custom_mapping_file: UploadFile = File(None, description="File that contains the user custom mapping file")):
     logger.info(
         f"POST request received for creating new project with id {id} and name {name} from Diagram {diag_type} file")
 
     logger.info("Parsing Diagram file to OTM")
-    otm_project = OtmProject.from_diag_file_to_otm_stream(id, name, diag_type, [diag_file.file],
-                                                          [mapping_file.file] if mapping_file else [])
+    otm_project = OtmProject.from_diag_file_to_otm_stream(
+        id, name, diag_type,
+        [diag_file.file],
+        [default_mapping_file.file, custom_mapping_file.file] if custom_mapping_file else [default_mapping_file.file]
+    )
 
     return Response(status_code=201, media_type="application/json", content=otm_project.get_otm_as_json())
