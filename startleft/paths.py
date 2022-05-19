bfc2ebdf728d69928a30e81ff35a4e2850581f98
David Antolin Alvarez <dantolin@iriusrisk.com>
2022-05-19 09:24:06 +0200

    [OPT-191] Refactored project resources structure

--- startleft/paths.py
+++ startleft/paths.py
@@ -2,10 +2,10 @@ import os
 
 path = os.path.dirname(__file__)
 
-default_cf_mapping_file = os.path.join(path, 'config/default-cloudformation-mapping.yaml')
-default_tf_mapping_file = os.path.join(path, 'config/default-terraform-mapping.yaml')
-default_visio_mapping_file = os.path.join(path, 'config/default-visio-mapping.yaml')
+default_cf_mapping_file = os.path.join(path, 'resources/defaultmappings/default-cloudformation-mapping.yaml')
+default_tf_mapping_file = os.path.join(path, 'resources/defaultmappings/default-terraform-mapping.yaml')
+default_visio_mapping_file = os.path.join(path, 'resources/defaultmappings/default-visio-mapping.yaml')
 
-otm_schema = os.path.join(path, 'data/otm_schema.json')
-iac_schema = os.path.join(path, 'data/iac_mapping_schema.json')
-diagram_schema = os.path.join(path, 'data/diagram_mapping_schema.json')
+otm_schema = os.path.join(path, 'resources/schemas/otm_schema.json')
+iac_schema = os.path.join(path, 'resources/schemas/iac_mapping_schema.json')
+diagram_schema = os.path.join(path, 'resources/schemas/diagram_mapping_schema.json')
