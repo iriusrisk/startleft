972601e0ab9113a067d84b1b002bf5d00ffac783
Santi Manero <smanero@iriusrisk.com>
2022-07-21 12:14:26 +0200

    [OPT-255] Deleted duplicated test methods

--- tests/integration/iac/test_iac_to_otm.py
+++ tests/integration/iac/test_iac_to_otm.py
@@ -21,18 +21,6 @@ class TestApp:
         iac_to_otm.load_yaml_source(FileUtils.get_data(filename))
         assert iac_to_otm.source_model.data
 
-    def test_load_yaml_uploaded_file(self):
-        filename = test_resource_paths.example_yaml
-        iac_to_otm = IacToOtm('name', 'id', IacType.CLOUDFORMATION)
-        iac_to_otm.load_yaml_source(open(filename))
-        assert iac_to_otm.source_model.data
-
-    def test_load_json_uploaded_file(self):
-        filename = test_resource_paths.example_json
-        iac_to_otm = IacToOtm('name', 'id', IacType.CLOUDFORMATION)
-        iac_to_otm.load_yaml_source(open(filename))
-        assert iac_to_otm.source_model.data
-
     def test_run(self):
         filename = test_resource_paths.example_json
         mapping_filename = test_resource_paths.default_cloudformation_mapping
