61767c6ea261ac5ce2a1457170b8c37eb33eb77c
Daniel Font <dfont@iriusrisk.com>
2022-08-16 13:03:43 +0200

    [OPT-257] Added unit test for cli parser iac, cli mutual exclusions for iac type and cli errors for iac files.

--- tests/unit/cli/parse/diagram/test_unit_cli_parse_diagram.py
+++ tests/unit/cli/parse/diagram/test_unit_cli_parse_diagram.py
@@ -18,7 +18,7 @@ class TestCliParseDiagram:
     @patch('startleft.cli.parse_diagram')
     def test_parse_diagram_file_ok(self, mock, caplog):
         """
-        Parsing Cloudformation file wih a valid mapping file
+        Parsing Diagram file with a valid mapping file
         """
         runner = CliRunner()
         output_file_name = "output-file.otm"
@@ -47,3 +47,38 @@ class TestCliParseDiagram:
             assert result.exit_code == 0
             assert 'Parsing source files into OTM' in caplog.text
             mock.assert_called_once()
+
+    def test_parse_diagram_mutually_exclusion_error(self):
+        """
+        Parsing Diagram file with an invalid commands options
+        """
+        runner = CliRunner()
+        output_file_name = "output-file.otm"
+
+        with runner.isolated_filesystem():
+            # Given a list of arguments with
+            args = [
+                # a valid Diagram type
+                '--diagram-type', "VISIO",
+                # a valid IaC type
+                '--iac-type', "CLOUDFORMATION",
+                #   and a valid mapping file
+                '--default-mapping-file', VISIO_DEFAULT_VALID_MAPPING_FILENAME,
+                #   and a valid project name
+                '--project-name', "project-name",
+                #   and a valid project id
+                '--project-id', "project-id",
+                #   and a valid output file name
+                '--output-file', output_file_name,
+                #   and a valid input file
+                VISIO_DIAGRAM_AWS_WITH_TZ_AND_VPC]
+
+            # When parsing
+            result = runner.invoke(parse_any, args)
+
+            # Then validator OTM file is generated
+            assert result.exit_code == 2
+
+            assert result.stdout.__contains__("Error: Invalid arguments: diagram_type is incompatible with:")
+            assert result.stdout.__contains__("mapping_file")
+            assert result.stdout.__contains__("iac_type")
