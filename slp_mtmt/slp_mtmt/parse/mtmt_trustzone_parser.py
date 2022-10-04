2144fa081352d3ff5b7d726c04c48d970141ed8e
Guillem Albiach <galbiach@iriusrisk.com>
2022-10-04 16:11:12 +0200

    [OPT-228] Fix merge conflict

--- slp_mtmt/slp_mtmt/parse/mtmt_trustzone_parser.py
+++ slp_mtmt/slp_mtmt/parse/mtmt_trustzone_parser.py
@@ -30,5 +30,3 @@ class MTMTTrustzoneParser:
         return self.mapping.mapping_trustzones[label]['type'] if label in self.mapping.mapping_trustzones \
             else None
 
-    def parse_default_trustzone(self) -> Trustzone:
-        return Trustzone("b61d6911-338d-46a8-9f39-8dcd24abfe91", "Public Cloud")
