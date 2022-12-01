68d1fae9839431442173f6ff27916b9ae2044e92
Santi Manero <smanero@iriusrisk.com>
2022-12-01 15:03:39 +0100

    [OPT-521] Restored repr in DiagramComponent

--- slp_visio/slp_visio/load/objects/diagram_objects.py
+++ slp_visio/slp_visio/load/objects/diagram_objects.py
@@ -34,8 +34,9 @@ class DiagramComponent:
                + 'parent_id: ' + str(self.parent.id if self.parent else None) + '}'
 
     def __repr__(self) -> str:
-        return self.__str__()
-
+        return '{id: ' + str(self.id) + ', ' \
+               + 'name: ' + self.name + ', ' \
+               + 'parent_id: ' + str(self.parent.id if self.parent else None) + '}'
 
 
 class DiagramConnector:
