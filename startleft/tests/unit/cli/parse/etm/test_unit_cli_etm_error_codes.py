6859c42fd6acff35b7b7858db6fa09c6b02eb5de
Daniel Font <dfont@iriusrisk.com>
2023-02-01 20:20:37 +0100

    [OPT-687] Add test for ProviderNotFoundError cli error code.

--- startleft/tests/unit/cli/parse/etm/test_unit_cli_etm_error_codes.py
+++ startleft/tests/unit/cli/parse/etm/test_unit_cli_etm_error_codes.py
@@ -3,7 +3,7 @@ from pytest import mark
 
 from click.testing import CliRunner
 
-from slp_base.slp_base.errors import MappingFileNotValidError, SourceFileNotValidError
+from slp_base.slp_base.errors import MappingFileNotValidError, SourceFileNotValidError, LoadingSourceFileError
 from startleft.startleft.cli.cli import parse_any
 from startleft.tests.resources import test_resource_paths
 
@@ -89,3 +89,35 @@ class TestCliEtmErrorCodes:
             # THEN a 52 exit code is returned
             assert result.exit_code == 1
             assert result.exception.error_code.system_exit_status == 52
+
+    @patch('slp_mtmt.slp_mtmt.mtmt_processor.MTMTProcessor.process')
+    def test_etm_loading_source_file_error(self, mock_load_source_data):
+        runner = CliRunner()
+        output_file_name = "output-file.otm"
+
+        error = LoadingSourceFileError('Source file cannot be loaded', None, None)
+
+        mock_load_source_data.side_effect = error
+
+        with runner.isolated_filesystem():
+            # Given a list of arguments with
+            args = [
+                # a valid MTMT type
+                '--etm-type', TESTING_ETM_TYPE,
+                #   and a valid mapping file
+                '--default-mapping-file', TESTING_MTMT_DEFAULT_VALID_MAPPING_FILENAME,
+                #   and a valid project name
+                '--project-name', "project-name",
+                #   and a valid project id
+                '--project-id', "project-id",
+                #   and a valid output file name
+                '--output-file', output_file_name,
+                #  and an invalid MTMT file
+                TESTING_MTMT_INVALID_FILE]
+
+            # When parse command of the CLI is called
+            result = runner.invoke(parse_any, args)
+
+            # THEN a 52 exit code is returned
+            assert result.exit_code == 1
+            assert result.exception.error_code.system_exit_status == 51
