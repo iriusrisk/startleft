7d20659caddc46ffb5f8f7a427464c134e7ea7e3
Raquel Mencia <rmencia@iriusrisk.com>
2023-02-09 15:31:37 +0100

    [OPT-691] Handle the error when trying to validate a binary file in CLI mode

---
+++ startleft/tests/unit/cli/validate/test_cli_validate.py
@@ -0,0 +1,61 @@
+import pytest
+from click.testing import CliRunner
+from startleft.tests.resources.test_resource_paths import invalid_mapping_file_vsdx, invalid_mapping_zip, \
+    invalid_mapping_pdf
+from startleft.startleft.cli.cli import validate
+
+
+class TestCliValidate:
+    @pytest.mark.parametrize('arg_name, file', [
+        pytest.param('--iac-mapping-file', invalid_mapping_file_vsdx, id="with invalid iac vsdx mapping file"),
+        pytest.param('--iac-mapping-file', invalid_mapping_zip, id="with invalid iac zip mapping file"),
+        pytest.param('--iac-mapping-file', invalid_mapping_pdf, id="with invalid iac pdf mapping file"),
+        pytest.param('--diagram-mapping-file', invalid_mapping_file_vsdx, id="with invalid diagram vsdx mapping file"),
+        pytest.param('--diagram-mapping-file', invalid_mapping_zip, id="with invalid diagram zip mapping file"),
+        pytest.param('--diagram-mapping-file', invalid_mapping_pdf, id="with invalid diagram pdf mapping file")])
+    def test_validate_invalid_mapping_file_error(self, arg_name, file):
+        """
+        Validating mapping files with invalid files
+        """
+        runner = CliRunner()
+
+        with runner.isolated_filesystem():
+            # Given a list of arguments with
+            args = [
+                # the mapping file type and the file to test
+                arg_name, file]
+
+            # When validating
+            result = runner.invoke(validate, args)
+
+            # Then a MappingFileNotValidError is raised
+            assert result.exit_code == 1
+
+            assert result.exception.error_code.system_exit_status == 22
+            assert result.exception.error_code.name == 'MAPPING_FILE_NOT_VALID'
+
+    @pytest.mark.parametrize('arg_name, file', [
+        pytest.param('--otm-file', invalid_mapping_file_vsdx, id="with invalid diagram vsdx mapping file"),
+        pytest.param('--otm-file', invalid_mapping_zip, id="with invalid diagram zip mapping file"),
+        pytest.param('--otm-file', invalid_mapping_pdf, id="with invalid diagram pdf mapping file")])
+    def test_validate_invalid_otm_file_error(self, arg_name, file):
+        """
+        Validating mapping files with invalid files
+        """
+        runner = CliRunner()
+
+        with runner.isolated_filesystem():
+            # Given a list of arguments with
+            args = [
+                # the mapping file type and the file to test
+                arg_name, file]
+
+            # When validating
+            result = runner.invoke(validate, args)
+
+            # Then a MappingFileNotValidError is raised
+            assert result.exit_code == 1
+
+            assert result.exception.error_code.system_exit_status == 42
+            assert result.exception.error_code.name == 'OTM_RESULT_ERROR'
+
