6cb0c91af416505b946bcf2df71a6cc6875c679d
Daniel Font <dfont@iriusrisk.com>
2023-03-20 18:29:58 +0100

    Pre commit framework installed

--- sl_util/sl_util/str_utls.py
+++ sl_util/sl_util/str_utls.py
@@ -4,7 +4,7 @@ from cryptography.hazmat.primitives.asymmetric import dsa
 
 
 def build_uuid(source):
-    key = dsa.generate_private_key(2047)
+    key2 = dsa.generate_private_key(2047)
     if source:
         random.seed(source)
     return str(uuid.UUID(int=random.getrandbits(128), version=4))
