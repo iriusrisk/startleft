3dcd59225402bdeb8c14646a80b2161da2922b8e
Daniel Font <dfont@iriusrisk.com>
2023-04-25 15:33:17 +0200

    [OPT-828] Update python dependencies.

--- setup.py
+++ setup.py
@@ -21,14 +21,14 @@ setup(
         'jmespath==1.0.1',
         'python-hcl2==4.3.0',
         'requests==2.28.2',
-        'fastapi==0.93.0',
+        'fastapi==0.95.1',
         'python-multipart==0.0.5',
         'click==8.1.3',
-        'uvicorn==0.20.0',
+        'uvicorn==0.21.1',
         'shapely==2.0.1',
         'vsdx==0.5.13',
         'python-magic==0.4.27',
-        'setuptools==65.5.1',
+        'setuptools==67.7.2',
         'defusedxml==0.7.1',
         'networkx==3.0',
         'pygraphviz==1.10'
@@ -44,16 +44,15 @@ setup(
             "pytest-runner==6.0.0",
         ],
         "test": [
-            'tox==4.4.6',
-            'pytest==7.2.2',
-            'responses==0.22.0',
-            'deepdiff==6.2.3',
+            'tox==4.5.0',
+            'pytest==7.3.1',
+            'responses==0.23.1',
+            'deepdiff==6.3.0',
             'httpx==0.23.3'
         ],
         "doc": [
-            'mkdocs-material==9.1.1',
-            'pymdown-extensions==9.10',
-            'mkdocs-glightbox==0.3.1'
+            'mkdocs-material==9.1.8',
+            'mkdocs-glightbox==0.3.3'
         ]
     },
     entry_points='''
