3443d5f5799d2d811f02c8f89327bad0c1fe0b23
Santi Manero <smanero@iriusrisk.com>
2023-07-11 12:38:52 +0200

    [OPT-765] Commented visio identifier classes purposes

--- slp_visio/slp_visio/load/boundary_identifier.py
+++ slp_visio/slp_visio/load/boundary_identifier.py
@@ -4,6 +4,10 @@ from slp_visio.slp_visio.load.strategies.boundary.boundary_identifier_strategy i
 
 
 class BoundaryIdentifier:
+    """
+    Identifies if a Visio Shape is a component of DiagramComponentOrigin.BOUNDARY type
+    when we parse from Visio Shape to a DiagramComponent class
+    """
 
     @staticmethod
     def is_boundary(shape: Shape) -> bool:
