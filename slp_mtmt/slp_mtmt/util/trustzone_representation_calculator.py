5e74eab276ff1b0f642acb558023bd72cffc2739
David Antolín <99404665+dantolin-iriusrisk@users.noreply.github.com>
2024-07-16 15:14:22 +0200

    [feature/BLAZ-202] to dev (#381)
    * [BLAZ-202] Only first MTMT is processed and representation is calculated for line boundaries
    
    * [BLAZ-202] Fixed wrong import
    
    * [BLAZ-202] Fixed wrong parent calculation in complex nesting scenarios
    
    * [BLAZ-202] Fixed setuptools-scm problem with last git version

--- slp_mtmt/slp_mtmt/util/trustzone_representation_calculator.py
+++ slp_mtmt/slp_mtmt/util/trustzone_representation_calculator.py
@@ -5,14 +5,18 @@ from slp_mtmt.slp_mtmt.util.representation_calculator import RepresentationCalcu
 class TrustzoneRepresentationCalculator(RepresentationCalculator):
 
     def get_position(self) -> (int, int):
-        if isinstance(self.element, MTMBorder):
-            return self.__get_border_position()
-        return None, None
+        if not isinstance(self.element, MTMBorder):
+            return None, None
+
+        return self.__get_relative_position() if isinstance(self.parent, MTMBorder) else self.__get_absolute_position()
 
     def get_size(self) -> (int, int):
         if isinstance(self.element, MTMBorder):
             return self.element.width, self.element.height
         return None, None
 
-    def __get_border_position(self):
+    def __get_absolute_position(self):
         return self.element.left, self.element.top
+
+    def __get_relative_position(self):
+        return self.element.left - self.parent.left, self.element.top - self.parent.top
